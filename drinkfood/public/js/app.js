43fd42df55e7df37bb046c067e6038b0ac26bd87
trphuongnam <phuongnamgroupers@gmail.com>
2021-05-24 22:36:41 +0700

    design product detail page

--- drinkfood/public/js/app.js
+++ drinkfood/public/js/app.js
@@ -23,3 +23,41 @@ function show_language()
     }
 }
 /* End: Function show language */
+
+function rating(star)
+{
+    switch (star) {
+        case 1:
+            document.getElementById('rating_1').style.color = "yellow";
+            break;
+        case 2:
+            document.getElementById('rating_1').style.color = "yellow";
+            document.getElementById('rating_2').style.color = "yellow";
+            break;
+        case 3:
+            document.getElementById('rating_1').style.color = "yellow";
+            document.getElementById('rating_2').style.color = "yellow";
+            document.getElementById('rating_3').style.color = "yellow";
+            break;
+        case 4:
+            document.getElementById('rating_1').style.color = "yellow";
+            document.getElementById('rating_2').style.color = "yellow";
+            document.getElementById('rating_3').style.color = "yellow";
+            document.getElementById('rating_4').style.color = "yellow";
+            break;
+        case 5:
+            document.getElementById('rating_1').style.color = "yellow";
+            document.getElementById('rating_2').style.color = "yellow";
+            document.getElementById('rating_3').style.color = "yellow";
+            document.getElementById('rating_5').style.color = "yellow";
+            break;
+                
+        default:
+            document.getElementById('rating_1').style.color = "white";
+            document.getElementById('rating_2').style.color = "white";
+            document.getElementById('rating_3').style.color = "white";
+            document.getElementById('rating_5').style.color = "white";
+            break;
+    }
+    //alert("Đánh gia : "+star+" sao");
+}
